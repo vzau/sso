2da2051aa916141ddfdeea24efabf3d8f217b54a
Daniel Hawton <daniel@hawton.org>
2021-06-14 19:43:08 -0500

    Add more debug messages

--- controllers/v1/token.go
+++ controllers/v1/token.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/dhawton/log4g"
 	"github.com/gin-gonic/gin"
+	"github.com/goccy/go-json"
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
@@ -34,19 +35,27 @@ type TokenResponse struct {
 }
 
 func PostToken(c *gin.Context) {
+	params, _ := json.Marshal(c.Params)
+	log4g.Category("controllers/authorize").Debug(string(params))
+	form, _ := json.Marshal(c.Request.PostForm)
+	log4g.Category("controllers/authorize").Debug(string(form))
+
 	treq := TokenRequest{}
 	if err := c.ShouldBind(&treq); err != nil {
+		log4g.Category("controllers/token").Error("Invalid request, missing field(s)")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid_request"})
 		return
 	}
 
 	if treq.GrantType != "authorization_code" {
+		log4g.Category("controllers/token").Error("Grant type is not authorization code")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid_grant"})
 		return
 	}
 
 	login := models.OAuthLogin{}
 	if err := models.DB.Where("code = ?", treq.Code).First(&login).Error; err != nil {
+		log4g.Category("controllers/token").Error(fmt.Sprintf("Code %s not found", treq.Code))
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid_request"})
 		return
 	}
@@ -54,12 +63,14 @@ func PostToken(c *gin.Context) {
 	defer models.DB.Delete(&login)
 
 	if treq.ClientId != login.Client.ClientId || treq.ClientSecret != login.Client.ClientSecret {
+		log4g.Category("controllers/token").Error(fmt.Sprintf("Invalid client: %s %s", treq.ClientId, treq.ClientSecret))
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid_client"})
 		return
 	}
 
 	hash := sha256.Sum256([]byte(treq.CodeVerifier))
 	if login.CodeChallenge != base64.RawURLEncoding.EncodeToString(hash[:]) {
+		log4g.Category("controllers/token").Error(fmt.Sprintf("Code Challenge failed"))
 		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid_grant"})
 		return
 	}
