c441bfb9522c8977aaf2453a8e127b379e5e8c63
Daniel Hawton <daniel@hawton.org>
2021-06-17 19:00:56 -0500

    add support for basic auth

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"unsafe"
 )
 
 func Getenv(key string, defaultValue string) string {
@@ -10,4 +11,19 @@ func Getenv(key string, defaultValue string) string {
 		return defaultValue
 	}
 	return val
+}
+
+// StringToBytes converts string to byte slice without a memory allocation.
+func StringToBytes(s string) []byte {
+	return *(*[]byte)(unsafe.Pointer(
+		&struct {
+			string
+			Cap int
+		}{s, len(s)},
+	))
+}
+
+// BytesToString converts byte slice to string without a memory allocation.
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
 }
